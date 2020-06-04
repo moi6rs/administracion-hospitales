eb54a09d25aee87fe27b9759fa6b38fc26c8f09d
mramoss <moi6ramoss@gmail.com>
2020-06-04 11:03:41 -0400

    proyecto Spring boot con Spring Initializr

---
+++ apiadmhospitales/src/main/java/com/dh/apiadmhospitales/ApiadmhospitalesApplication.java
@@ -0,0 +1,13 @@
+package com.dh.apiadmhospitales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiadmhospitalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiadmhospitalesApplication.class, args);
+    }
+
+}
---
+++ apiadmhospitales/src/test/java/com/dh/apiadmhospitales/ApiadmhospitalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.apiadmhospitales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiadmhospitalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
